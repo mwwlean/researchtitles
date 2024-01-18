05a5aa9ebbd31723db9fe27e6c45078bd9d9e3eb
mwwlean <foolsparadise984@gmail.com>
2024-01-18 16:11:32 +0800

    fix bug

--- researchtitle_generator/src/routes/+page.svelte
+++ researchtitle_generator/src/routes/+page.svelte
@@ -2,8 +2,6 @@
     import Navbar from "/src/components/Navbar.svelte";
     import Hero from "/src/components/Hero.svelte";
     import Generate from "/src/components/Generate.svelte";
-    import Footer from "/src/components/Footer.svelte";
-    
 </script>
 
 <Navbar/>
